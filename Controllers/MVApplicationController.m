be0b294ef65be086375c674d6dbd3304947de851
= <zacharydrayer@gmail.com>
2010-01-09 01:11:18 +0000

    Fix a string for localization, use []'s instead of .'s

--- Controllers/MVApplicationController.m
+++ Controllers/MVApplicationController.m
@@ -516,8 +516,8 @@ static BOOL applicationIsTerminating = NO;
 	[self performSelector:@selector( setupFolders ) withObject:nil afterDelay:5.]; // do this later to speed up launch
 
 	if (![[NSUserDefaults standardUserDefaults] boolForKey:@"JVAskedToAllowAnalytics"]) {
-		NSAlert *alert = [NSAlert alertWithMessageText:NSLocalizedString(@"Allow analytics to be sent?", @"Allow analytics to be sent? message text.") defaultButton:NSLocalizedString(@"Send", @"Send button title") alternateButton:NSLocalizedString(@"Don't send", @"Don't send button title") otherButton:nil informativeTextWithFormat:NSLocalizedString(@"To help us know what to improve on, Colloquy can send back information about your current configuration. The data sent back will not contain will not contain any identifiable information. ", @"To help us know what to improve on, Voltage can send back information about your current configuration. The data sent back will not contain will not contain any identifiable information message text")];
-		alert.alertStyle = NSInformationalAlertStyle;
+		NSAlert *alert = [NSAlert alertWithMessageText:NSLocalizedString(@"Allow analytics to be sent?", @"Allow analytics to be sent? message text.") defaultButton:NSLocalizedString(@"Send", @"Send button title") alternateButton:NSLocalizedString(@"Don't send", @"Don't send button title") otherButton:nil informativeTextWithFormat:NSLocalizedString(@"To help us know what to improve on, Colloquy can send back information about your current configuration. The data sent back will not contain will not contain any identifiable information. ", @"To help us know what to improve on, Colloquy can send back information about your current configuration. The data sent back will not contain will not contain any identifiable information message text")];
+		[alert setAlertStyle:NSInformationalAlertStyle];
 
 		if ([alert runModal])
 			[[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"JVAllowAnalytics"];
