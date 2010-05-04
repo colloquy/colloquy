ff6a28b2fa5356f69139f8bc074f51c920531ea7
= <zacharydrayer@gmail.com>
2010-05-04 06:44:04 +0000

    Don't migrate old preferences over anymore

--- main.m
+++ main.m
@@ -11,15 +11,5 @@ int main( int count, const char *arg[] ) {
 	xmlSubstituteEntitiesDefault( 1 );
 	xmlLoadExtDtdDefaultValue = 1;
 
-	NSAutoreleasePool *pool = [[NSAutoreleasePool allocWithZone:nil] init];
-
-	NSString *newPreferencesPath = [@"~/Library/Preferences/info.colloquy.plist" stringByExpandingTildeInPath];
-	if( ! [[NSFileManager defaultManager] fileExistsAtPath:newPreferencesPath] ) {
-		NSString *oldPreferencesPath = [@"~/Library/Preferences/cc.javelin.colloquy.plist" stringByExpandingTildeInPath];
-		[[NSFileManager defaultManager] movePath:oldPreferencesPath toPath:newPreferencesPath handler:nil];
-	}
-
-	[pool release];
-
 	return NSApplicationMain( count, arg );
 }
