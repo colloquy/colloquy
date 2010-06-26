0db1cf334eff5d96333861ac5f9384a6862f5160
= <zacharydrayer@gmail.com>
2010-06-26 08:00:21 +0000

    Reload tableViews when entering the foreground

--- Mobile/Controllers/CQTableViewController.m
+++ Mobile/Controllers/CQTableViewController.m
@@ -1,14 +1,29 @@
 #import "CQTableViewController.h"
 
 @implementation CQTableViewController
+- (id) initWithStyle:(UITableViewStyle) style {
+	if (!(self = [super initWithStyle:style]))
+		return nil;
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_4_0
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_willEnterForeground) name:UIApplicationWillEnterForegroundNotification object:nil];
+#endif
+
+	return self;
+}
+
 - (void) dealloc {
 	if ([self isViewLoaded]) {
 		self.tableView.dataSource = nil;
 		self.tableView.delegate = nil;
 	}
 
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+
 	[super dealloc];
 }
+
+#pragma mark -
 
 - (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation) interfaceOrientation {
 	if (interfaceOrientation == UIInterfaceOrientationPortrait)
@@ -16,5 +31,12 @@
 	if (![[UIDevice currentDevice] isPadModel] && interfaceOrientation == UIDeviceOrientationPortraitUpsideDown)
 		return NO;
 	return ![[NSUserDefaults standardUserDefaults] boolForKey:@"CQDisableLandscape"];
+}
+
+#pragma mark -
+
+- (void) _willEnterForeground {
+	if ([self isViewLoaded])
+		[self.tableView reloadData];
 }
 @end
