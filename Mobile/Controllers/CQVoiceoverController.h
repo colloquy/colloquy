0da5d37f9adaa7fa4ca5e5c035b229f782f26915
= <zacharydrayer@gmail.com>
2010-07-15 06:55:04 +0000

    Remove CQVoiceoverController

--- Mobile/Controllers/CQVoiceoverController.h
+++
@@ -1,6 +0,0 @@
-@interface CQVoiceoverController : NSObject
-+ (CQVoiceoverController *) defaultController;
-
-- (void) postNotificationWithMessage:(NSString *) message user:(NSString *) user channel:(NSString *) channel; // For highlights in background channels
-- (void) postNotificationWithMessage:(NSString *) message user:(NSString *) user privately:(BOOL) privately; // For the current channel or in a privmsg/notice
-@end
