095f3102fbc0035bfce3546956454380d33ec79f
= <zacharydrayer@gmail.com>
2010-12-27 22:28:53 +0000

    Set images for different mouse states in button cells

--- Views/CQDualButtonCell.h
+++ Views/CQDualButtonCell.h
@@ -1,15 +1,25 @@
+extern NSString *CQDualButtonLeftDictionaryKey;
+extern NSString *CQDualButtonRightDictionaryKey;
+
+extern NSString *CQMouseStateDefaultKey;
+extern NSString *CQMouseStateHoverKey;
+extern NSString *CQMouseStateClickKey;
+
 @interface CQDualButtonCell : NSCell {
 @private
 	NSButtonCell *_leftButtonCell;
 	NSButtonCell *_rightButtonCell;
 	BOOL _hideLeftButton;
 
-	BOOL _mouseoverInLeftButton;
-	BOOL _mouseoverInRightButton;
+	NSInteger _leftButtonMouseState;
+	NSInteger _rightButtonMouseState;
+
+	NSDictionary *_mouseStates;
 }
 @property (nonatomic, readonly) NSButtonCell *leftButtonCell;
 @property (nonatomic, readonly) NSButtonCell *rightButtonCell;
 @property (nonatomic) BOOL hidesLeftButton;
+@property (nonatomic, retain) NSDictionary *mouseStates;
 
 - (void) mouseEntered:(NSEvent *) event;
 - (void) mouseExited:(NSEvent *) event;
--- Views/CQDualButtonCell.m
+++ Views/CQDualButtonCell.m
@@ -1,5 +1,16 @@
 #import "CQDualButtonCell.h"
 
+#define CQMouseStateDefault 0
+#define CQMouseStateHover 1
+#define CQMouseStateClick 2
+
+NSString *CQDualButtonLeftDictionaryKey = @"CQDualButtonLeftDictionaryKey";
+NSString *CQDualButtonRightDictionaryKey = @"CQDualButtonRightDictionaryKey";
+
+NSString *CQMouseStateDefaultKey = @"CQMouseStateDefaultKey";
+NSString *CQMouseStateHoverKey = @"CQMouseStateHoverKey";
+NSString *CQMouseStateClickKey = @"CQMouseStateClickKey";
+
 @interface NSButtonCell (Properties)
 @property (nonatomic) NSButtonType buttonType;
 @property (nonatomic) BOOL bordered;
@@ -12,12 +23,15 @@
 @synthesize rightButtonCell = _rightButtonCell;
 @synthesize leftButtonCell = _leftButtonCell;
 @synthesize hidesLeftButton = _hideLeftButton;
+@synthesize mouseStates = _mouseStates;
 
 #pragma mark -
 
 + (BOOL) prefersTrackingUntilMouseUp {
 	return YES;
 }
+
+#pragma mark -
 
 - (id) init {
 	if (!(self = [super init]))
@@ -29,7 +43,7 @@
 	_rightButtonCell.imagePosition = NSImageRight;
 	_rightButtonCell.bordered = NO;
 	_rightButtonCell.continuous = NO;
-	[_rightButtonCell sendActionOn:NSLeftMouseUp];;
+	[_rightButtonCell sendActionOn:NSLeftMouseUp];
 
 	_leftButtonCell = [[NSButtonCell alloc] init];
 	_leftButtonCell.buttonType = NSSwitchButton;
@@ -37,7 +51,7 @@
 	_leftButtonCell.imagePosition = NSImageRight;
 	_leftButtonCell.bordered = NO;
 	_leftButtonCell.continuous = NO;
-	[_leftButtonCell sendActionOn:NSLeftMouseUp];;
+	[_leftButtonCell sendActionOn:NSLeftMouseUp];
 
 	return self;
 }
@@ -47,19 +61,27 @@
 	cell->_rightButtonCell = [_rightButtonCell retain];
 	cell->_leftButtonCell = [_leftButtonCell retain];
 	cell->_hideLeftButton = _hideLeftButton;
-	cell->_mouseoverInLeftButton = _mouseoverInLeftButton;
-	cell->_mouseoverInRightButton = _mouseoverInRightButton;
+	cell->_leftButtonMouseState = _leftButtonMouseState;
+	cell->_rightButtonMouseState = _rightButtonMouseState;
+	cell->_mouseStates = [_mouseStates retain];
 	return cell;
 }
 
 - (void) dealloc {
+	[_mouseStates release];
 	[_rightButtonCell release];
 	[_leftButtonCell release];
-	
+
 	[super dealloc];
 }
 
 #pragma mark -
+
+- (void) setHidesLeftButton:(BOOL) hidesLeftButton {
+	_hideLeftButton = hidesLeftButton;
+
+	[(NSControl *)self.controlView updateCell:self];
+}
 
 - (NSRect) _rightButtonCellFrameFromRect:(NSRect) cellFrame {
 #define rightButtonBorderMargin 9.
@@ -80,13 +102,34 @@
 
 #pragma mark -
 
+- (NSImage *) _imageFromState:(NSInteger) state inDictionary:(NSDictionary *) dictionary {
+	if (!dictionary)
+		return nil;
+
+	switch (_leftButtonMouseState) {
+	case CQMouseStateDefault:
+		return [dictionary objectForKey:CQMouseStateDefaultKey];
+	case CQMouseStateHover:
+		return [dictionary objectForKey:CQMouseStateHoverKey];
+	case CQMouseStateClick:
+		return [dictionary objectForKey:CQMouseStateClickKey];
+	}
+
+	return nil;
+}
+
 - (void) drawWithFrame:(NSRect) cellFrame inView:(NSView *) controlView {
-	NSRect rightFrame = [self _rightButtonCellFrameFromRect:cellFrame];
-
-	[_rightButtonCell drawWithFrame:rightFrame inView:controlView];
-
-	if (!_hideLeftButton)
+	if (!_hideLeftButton) {
+		NSImage *image = [self _imageFromState:_leftButtonMouseState inDictionary:[_mouseStates objectForKey:CQDualButtonLeftDictionaryKey]];
+		if (image)
+			_leftButtonCell.image = image;
 		[_leftButtonCell drawWithFrame:[self _leftButtonCellFrameFromRect:cellFrame] inView:controlView];
+	}
+
+	NSImage *image = [self _imageFromState:_rightButtonMouseState inDictionary:[_mouseStates objectForKey:CQDualButtonRightDictionaryKey]];
+	if (image)
+		_rightButtonCell.image = image;
+	[_rightButtonCell drawWithFrame:[self _rightButtonCellFrameFromRect:cellFrame] inView:controlView];
 }
 
 - (NSUInteger) hitTestForEvent:(NSEvent *) event inRect:(NSRect) cellFrame ofView:(NSView *) controlView {
@@ -94,25 +137,23 @@
 
 	if (!_hideLeftButton && NSMouseInRect(point, [self _leftButtonCellFrameFromRect:cellFrame], [controlView isFlipped]))
 		return (NSCellHitContentArea | NSCellHitTrackableArea);
-
 	if (NSMouseInRect(point, [self _rightButtonCellFrameFromRect:cellFrame], [controlView isFlipped]))
 		return (NSCellHitContentArea | NSCellHitTrackableArea);
-
 	return [super hitTestForEvent:event inRect:cellFrame ofView:controlView];
 }
 
 #pragma mark -
 
 - (void) _addTrackingAreasForView:(NSView *) controlView inRect:(NSRect) cellFrame withUserInfo:(NSDictionary *) userInfo mouseLocation:(NSPoint) mouseLocation forButtonFrame:(NSRect) buttonFrame {
-    NSTrackingAreaOptions options = NSTrackingEnabledDuringMouseDrag | NSTrackingMouseEnteredAndExited | NSTrackingActiveAlways;
+	NSTrackingAreaOptions options = (NSTrackingEnabledDuringMouseDrag | NSTrackingMouseEnteredAndExited | NSTrackingActiveAlways);
 
-    if (NSMouseInRect(mouseLocation, buttonFrame, [controlView isFlipped])) {
-        options |= NSTrackingAssumeInside;
-        [controlView setNeedsDisplayInRect:cellFrame];
-    }
+	if (NSMouseInRect(mouseLocation, buttonFrame, [controlView isFlipped])) {
+		options |= NSTrackingAssumeInside;
+		[controlView setNeedsDisplayInRect:cellFrame];
+	}
 
-    NSTrackingArea *buttonTrackingArea = [[NSTrackingArea alloc] initWithRect:buttonFrame options:options owner:controlView userInfo:userInfo];
-    [controlView addTrackingArea:buttonTrackingArea];
+	NSTrackingArea *buttonTrackingArea = [[NSTrackingArea alloc] initWithRect:buttonFrame options:options owner:controlView userInfo:userInfo];
+	[controlView addTrackingArea:buttonTrackingArea];
 	[buttonTrackingArea release];
 }
 
@@ -129,18 +170,59 @@
 	[modifiedUserInfo release];
 }
 
+#pragma mark -
+
+- (BOOL)trackMouse:(NSEvent *) mouseEvent inRect:(NSRect) cellFrame ofView:(NSView *) controlView untilMouseUp:(BOOL) untilMouseUp {
+	self.controlView = controlView;
+
+	NSRect leftButtonRect = [self _leftButtonCellFrameFromRect:cellFrame];
+	NSRect rightButtonRect = [self _rightButtonCellFrameFromRect:cellFrame];
+
+	while (mouseEvent.type != NSLeftMouseUp) {
+		NSPoint point = [controlView convertPoint:[mouseEvent locationInWindow] fromView:nil];
+		if (NSMouseInRect(point, leftButtonRect, [controlView isFlipped]) && (_leftButtonMouseState != CQMouseStateClick)) {
+			_leftButtonMouseState = CQMouseStateClick;
+			[controlView setNeedsDisplayInRect:cellFrame];
+		}
+
+		if (NSMouseInRect(point, rightButtonRect, [controlView isFlipped]) && (_rightButtonMouseState != CQMouseStateClick)) {
+			_rightButtonMouseState = CQMouseStateClick;
+			[controlView setNeedsDisplayInRect:cellFrame];
+		}
+
+		if (mouseEvent.type == NSMouseEntered || mouseEvent.type == NSMouseExited)
+			[NSApp sendEvent:mouseEvent];
+
+		mouseEvent = [controlView.window nextEventMatchingMask:(NSLeftMouseUpMask | NSLeftMouseDraggedMask | NSMouseEnteredMask | NSMouseExitedMask)];
+	}
+
+	if (_leftButtonMouseState == CQMouseStateClick) {
+		[_leftButtonCell trackMouse:mouseEvent inRect:leftButtonRect ofView:controlView untilMouseUp:untilMouseUp];
+		_leftButtonMouseState = CQMouseStateHover;
+		[controlView setNeedsDisplayInRect:cellFrame];
+	}
+
+	if (_rightButtonMouseState == CQMouseStateClick) {
+		[_rightButtonCell trackMouse:mouseEvent inRect:rightButtonRect ofView:controlView untilMouseUp:untilMouseUp];
+		_rightButtonMouseState = CQMouseStateHover;
+		[controlView setNeedsDisplayInRect:cellFrame];
+	}
+
+	return YES;
+}
+
 - (void) mouseEntered:(NSEvent *) event {
 	if ([[event.trackingArea.userInfo objectForKey:@"Position"] isEqualToString:@"Left"])
-		_mouseoverInLeftButton = YES;
-	else _mouseoverInRightButton = YES;
+		_leftButtonMouseState = CQMouseStateHover;
+	else _rightButtonMouseState = CQMouseStateHover;
 
 	[(NSControl *)self.controlView updateCell:self];
 }
 
 - (void) mouseExited:(NSEvent *) event {
 	if ([[event.trackingArea.userInfo objectForKey:@"Position"] isEqualToString:@"Left"])
-		_mouseoverInLeftButton = NO;
-	else _mouseoverInRightButton = NO;
+		_leftButtonMouseState = CQMouseStateDefault;
+	else _rightButtonMouseState = CQMouseStateDefault;
 
 	[(NSControl *)self.controlView updateCell:self];
 }
