dd6d2dbdb73b89ade2f1b6330f514b4e3fdc718f
= <zacharydrayer@gmail.com>
2011-03-30 04:43:29 +0000

    Remove Ruby Support; It was never added to Colloquy and MacRuby can/should be used instead.

--- Plug-Ins/Ruby Support/JVRubyPluginLoader.h
+++
@@ -1,8 +0,0 @@
-#import "MVChatPluginManager.h"
-
-@interface JVRubyPluginLoader : NSObject <MVChatPlugin> {
-	MVChatPluginManager *_manager;
-	BOOL _rubyCocoaInstalled;
-}
-- (void) loadPluginNamed:(NSString *) name;
-@end
