ad3e70be240e51b8d621c0d5742288cb91b3cc02
= <zacharydrayer@gmail.com>
2011-12-17 00:01:27 +0000

    Initialize char arrays to 0s, to silence the analyzer

--- Additions/NSDataAdditions.m
+++ Additions/NSDataAdditions.m
@@ -23,7 +23,7 @@ static char encodingTable[64] = {
 		unsigned long ixtext = 0;
 		unsigned long lentext = 0;
 		unsigned char ch = 0;
-		unsigned char inbuf[4], outbuf[3];
+		unsigned char inbuf[4] =  { 0, 0, 0, 0 }, outbuf[3] = { 0, 0, 0 };
 		short i = 0, ixinbuf = 0;
 		BOOL flignore = NO;
 		BOOL flendtext = NO;
