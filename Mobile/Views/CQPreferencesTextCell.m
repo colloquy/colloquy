c97aa56f7b4d40888a0cb21f69cab2b89952855a
= <zacharydrayer@gmail.com>
2013-02-03 10:58:02 +0000

    Never hide the textfield in a preference text cell.

--- Mobile/Views/CQPreferencesTextCell.m
+++ Mobile/Views/CQPreferencesTextCell.m
@@ -94,39 +94,32 @@ static CQPreferencesTextCell *currentEditingCell;
 	UILabel *label = self.textLabel;
 
 	BOOL showingLabel = (label.text.length > 0);
-	BOOL showingTextField = (_textField.text.length || _textField.placeholder.length);
 
 	if (showingLabel) {
 		label.hidden = NO;
 
 		CGRect frame = label.frame;
-		if (!showingTextField)
-			frame.size.width = (contentRect.size.width - frame.origin.x - 10.);
-		else frame.size.width = [label sizeThatFits:label.bounds.size].width;
+		frame.size.width = [label sizeThatFits:label.bounds.size].width;
 		label.frame = frame;
 	} else {
 		label.hidden = YES;
 	}
 
-	if (showingTextField) {
-		_textField.hidden = NO;
+	_textField.hidden = NO;
 
-		const CGFloat leftMargin = 10.;
-		CGFloat rightMargin = 10.;
-		if (_textField.clearButtonMode == UITextFieldViewModeAlways)
-			rightMargin = 0.;
-		else if (self.accessoryType == UITableViewCellAccessoryDisclosureIndicator)
-			rightMargin = 4.;
+	const CGFloat leftMargin = 10.;
+	CGFloat rightMargin = 10.;
+	if (_textField.clearButtonMode == UITextFieldViewModeAlways)
+		rightMargin = 0.;
+	else if (self.accessoryType == UITableViewCellAccessoryDisclosureIndicator)
+		rightMargin = 4.;
 
-		CGRect frame = _textField.frame;
-		NSAssert(frame.size.height > 0., @"A height is assumed to be set in initWithFrame:.");
-		frame.origin.x = (showingLabel ? MAX(CGRectGetMaxX(label.frame) + leftMargin, 125.) : leftMargin);
-		frame.origin.y = round((contentRect.size.height / 2.) - (frame.size.height / 2.));
-		frame.size.width = (contentRect.size.width - frame.origin.x - rightMargin);
-		_textField.frame = frame;
-	} else {
-		_textField.hidden = YES;
-	}
+	CGRect frame = _textField.frame;
+	NSAssert(frame.size.height > 0., @"A height is assumed to be set in initWithFrame:.");
+	frame.origin.x = (showingLabel ? MAX(CGRectGetMaxX(label.frame) + leftMargin, 125.) : leftMargin);
+	frame.origin.y = round((contentRect.size.height / 2.) - (frame.size.height / 2.));
+	frame.size.width = (contentRect.size.width - frame.origin.x - rightMargin);
+	_textField.frame = frame;
 }
 
 @synthesize enabled = _enabled;
