5f2aaab223b9c1be5da9689797337f0c76e6c613
= <zacharydrayer@gmail.com>
2013-09-09 01:16:48 +0000

    Cache a value that never changes

--- Additions/NSCharacterSetAdditions.m
+++ Additions/NSCharacterSetAdditions.m
@@ -2,14 +2,19 @@
 
 @implementation NSCharacterSet (Additions)
 + (NSCharacterSet *) illegalXMLCharacterSet {
-	NSMutableCharacterSet *illegalSet = [[NSCharacterSet characterSetWithRange:NSMakeRange( 0, 0x1f )] mutableCopy];
+	static NSMutableCharacterSet *illegalSet = nil;
+	if (!illegalSet) {
+		illegalSet = [[NSCharacterSet characterSetWithRange:NSMakeRange( 0, 0x1f )] mutableCopy];
 
-	[illegalSet removeCharactersInRange:NSMakeRange( 0x09, 1 )];
+		[illegalSet removeCharactersInRange:NSMakeRange( 0x09, 1 )];
 
-	[illegalSet addCharactersInRange:NSMakeRange( 0x7f, 1 )];
-	[illegalSet addCharactersInRange:NSMakeRange( 0xfffe, 1 )];
-	[illegalSet addCharactersInRange:NSMakeRange( 0xffff, 1 )];
+		[illegalSet addCharactersInRange:NSMakeRange( 0x7f, 1 )];
+		[illegalSet addCharactersInRange:NSMakeRange( 0xfffe, 1 )];
+		[illegalSet addCharactersInRange:NSMakeRange( 0xffff, 1 )];
 
-	return [illegalSet autorelease];
+		illegalSet = [illegalSet copy];
+	}
+
+	return [[illegalSet copy] autorelease];
 }
 @end
