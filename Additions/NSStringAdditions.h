538963547b338bf39fd2263b464558aeed606284
C.W. Betts <computers57@hotmail.com>
2016-06-12 13:53:45 -0600

    Quiet a warning.

--- Additions/NSStringAdditions.h
+++ Additions/NSStringAdditions.h
@@ -2,7 +2,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-BOOL isValidUTF8( const char *string, NSUInteger length );
+extern BOOL isValidUTF8( const char *string, NSUInteger length );
 
 #define is7Bit(ch) (((ch) & 0x80) == 0)
 #define isUTF8Tupel(ch) (((ch) & 0xE0) == 0xC0)
