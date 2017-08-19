31440c2d9612917310253a44f58152781db045ea
Alexander Kempgen <alex@kempgen.de>
2017-08-19 17:43:10 +0200

    Switch new chat-core framework to target ios instead

--- chat-core/chat-core/ChatCore.h
+++ chat-core/chat-core/ChatCore.h
@@ -15,8 +15,6 @@ FOUNDATION_EXPORT double ChatCoreVersionNumber;
 //! Project version string for ChatCore.
 FOUNDATION_EXPORT const unsigned char ChatCoreVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ChatCore/PublicHeader.h>
-
 
 //#import <ChatCore/MVAvailability.h>
 //#import <ChatCore/MVUtilities.h>
