de9b296f91e81b5805c282f81e5c9cbf43d42ce8
C.W. Betts <computers57@hotmail.com>
2017-10-23 00:49:03 -0600

    Remove commented-out code.

--- Additions/NSImageAdditions.m
+++ Additions/NSImageAdditions.m
@@ -4,40 +4,12 @@
 // Created for Adium by Evan Schoenberg on Tue Dec 02 2003 under the GPL.
 // Draw this image in a rect, tiling if the rect is larger than the image
 - (void) tileInRect:(NSRect) rect {
-#if 1
 	[[NSGraphicsContext currentContext] saveGraphicsState];
 	NSColor *aColor = [NSColor colorWithPatternImage:self];
 	[aColor set];
 	[[NSBezierPath bezierPathWithRect:rect] fill];
 	
 	[[NSGraphicsContext currentContext] restoreGraphicsState];
-#else
-	NSSize size = [self size];
-	NSRect destRect = NSMakeRect( rect.origin.x, rect.origin.y, size.width, size.height );
-	double top = rect.origin.y + rect.size.height;
-	double right = rect.origin.x + rect.size.width;
-
-	NSRect sourceRect = NSMakeRect( 0, 0, size.width, size.height );
-
-	// Tile vertically
-	while( destRect.origin.y < top ) {
-		// Tile horizontally
-		while( destRect.origin.x < right ) {
-			// Crop as necessary
-			if( ( destRect.origin.x + destRect.size.width ) > right )
-				sourceRect.size.width -= ( destRect.origin.x + destRect.size.width ) - right;
-
-			if( ( destRect.origin.y + destRect.size.height ) > top )
-				sourceRect.size.height -= ( destRect.origin.y + destRect.size.height ) - top;
-
-			// Draw and shift
-			[self drawAtPoint:destRect.origin fromRect:sourceRect operation:NSCompositeSourceOver fraction:1.0];
-			destRect.origin.x += destRect.size.width;
-		}
-
-		destRect.origin.y += destRect.size.height;
-	}
-#endif
 }
 
 + (NSImage *)templateName:(NSString *)templateName
