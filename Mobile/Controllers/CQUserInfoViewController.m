2584a1d2653d1c53dffc82acf337a4cfe0c6c7dc
Zachary Drayer <zacharydrayer@gmail.com>
2017-11-13 00:23:43 -0800

    fix formatting issue when showing connected time in whois

--- Mobile/Controllers/CQUserInfoViewController.m
+++ Mobile/Controllers/CQUserInfoViewController.m
@@ -181,7 +181,7 @@ NS_ASSUME_NONNULL_BEGIN
 			cell.textLabel.text = NSLocalizedString(@"connected", "Connected user info label");
 
 			if (_user.status != MVChatUserOfflineStatus && _user.dateConnected) {
-				cell.detailTextLabel.text = humanReadableTimeInterval([_user.dateConnected timeIntervalSinceNow]);
+				cell.detailTextLabel.text = humanReadableTimeInterval(-[_user.dateConnected timeIntervalSinceNow]);
 				cell.accessibilityLabel = [NSString stringWithFormat:NSLocalizedString(@"Connected: %@", @"Voiceover Connected label"), cell.detailTextLabel.text];
 			} else {
 				cell.detailTextLabel.text = NSLocalizedString(@"Offline", "Offline label");
@@ -276,7 +276,7 @@ NS_ASSUME_NONNULL_BEGIN
 	// Connected time
 	NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:3];
 	UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
-	if (cell) cell.detailTextLabel.text = (online ? humanReadableTimeInterval([_user.dateConnected timeIntervalSinceNow]) : NSLocalizedString(@"Offline", "Offline label"));
+	if (cell) cell.detailTextLabel.text = (online ? humanReadableTimeInterval(-[_user.dateConnected timeIntervalSinceNow]) : NSLocalizedString(@"Offline", "Offline label"));
 	[cell layoutSubviews];
 
 	// Idle time
