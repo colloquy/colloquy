52bd75290b2479ba348de42dfcef505dd0d0eaba
Zachary Drayer <zacharydrayer@gmail.com>
2020-06-27 04:31:37 -0700

    call super

--- Mobile/Controllers/CQModalNavigationController.m
+++ Mobile/Controllers/CQModalNavigationController.m
@@ -9,7 +9,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @implementation CQModalNavigationController
 - (instancetype) initWithRootViewController:(UIViewController *) rootViewController {
-	if (!(self = [self initWithRootViewController:rootViewController]))
+	if (!(self = [super initWithRootViewController:rootViewController]))
 		return nil;
 
 	_rootViewController = rootViewController;
